68db96e0d6d3a8c4f649e2826e5b6085d2a9afd6
kvdevelopuse <kvdevelopuse@gmail.com>
2026-09-26 15:41:09 +0400

    changes

--- module1/kiran_vamadevan_module1_8.py
+++ module1/kiran_vamadevan_module1_8.py
@@ -1,12 +1,12 @@
 def get_address_word_count():
-    with open('address_book.txt', 'r') as file:
+    with open('address_book.txt', 'r', encoding='utf-8') as file:
         lines = file.readlines()
 
         new_lines = []
         address_word_count = 0
 
         for line in lines:
-            if line.startswith("Address:"):
+            if line.startswith("Address"):
                 address = line.replace("Address", "").strip()
                 address_word_count = len(address.split())
 
@@ -16,7 +16,7 @@ def get_address_word_count():
                 new_lines.append("Address Word Count: " +
                                  str(address_word_count) + "\n")
 
-    with open("address_book.txt", "w") as file:
+    with open("address_book.txt", "w", encoding='utf-8') as file:
         file.writelines(new_lines)
 
 
